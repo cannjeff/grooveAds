0336d515c46ff6f043fff2e1ae2ef0f8fb44e9da
Jeff Cann <cannjeff@gmail.com>
2012-05-16 09:13:07 -0400

    First revision

--- removeAds.js
+++ removeAds.js
@@ -1,17 +1,1 @@
-var gs=function() {
-    if (document.getElementById('lightbox_overlay')!=null) {
-        var head = document.head;
-        if ("item" in head) {
-            if (!head[0]) {
-                setTimeout(gs, 25);
-                return;
-            }
-            head = head[0];
-        }
-        var scriptElem = document.createElement("script");
-        scriptElem.src = '';
-        head.insertBefore(scriptElem, head.firstChild);
-    } else {
-        setTimeout( gs, 500);
-    }
-};gs();
+console.log('caught ya!');
